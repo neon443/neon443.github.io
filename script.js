157ba749c00422169e127149913d4f330e816f29
Nihaal Sharma <neonsharma@hotmail.com>
2023-06-04 15:37:29 +0100

    VERY GOOD VERY NICE
    Bar changes:
      + each item takes up the same amnt of space
      + hover: item grows, others shrink
      + hover: text size increases
      + if a link, bg color + text color inverts
      + if not link, bg color is sligtly darker/ligher
      + if not link clicked, message shows
      + neon443 is font-size 30
    
    html changes:
      + removed ids for barItems, unnesccesary
      + spaced out baritem code to make more readable
    
    css changes:
      + added notlink toast
    
    js changes:
      + added not link toast

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function showAlert() {
-    alert("Hey! this is under development. i told u not to click.");
+	alert("Hey! this is under development. i told u not to click.");
+}
+
+function notLink() {
+	var notLinkToast = document.getElementById("notLinkToast");
+	notLinkToast.className = "show";
+	setTimeout(
+		function() {
+			x.className = x.className.replace("show", "");
+		}
+	, 3000);
 }
