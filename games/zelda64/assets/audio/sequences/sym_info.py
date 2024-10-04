1f334aa70a65eb6d7dcf32833bf15a118415b158
coolpro9828 <138903608+coolpro9828@users.noreply.github.com>
2024-10-04 13:14:43 +0100

    Add files via upload

---
+++ games/zelda64/assets/audio/sequences/sym_info.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import argparse
+from pathlib import Path
+
+import mapfile_parser
+
+
+def symInfoMain():
+    parser = argparse.ArgumentParser(description="Display various information about a symbol or address.")
+    parser.add_argument("symname", help="symbol name or VROM/VRAM address to lookup")
+    parser.add_argument("-v", "--version", dest="oot_version", help="Which version should be processed", default="gc-eu-mq-dbg")
+    parser.add_argument("-e", "--expected", dest="use_expected", action="store_true", help="use the map file in expected/build/ instead of build/")
+
+    args = parser.parse_args()
+
+    BUILTMAP = Path("build") / args.oot_version / f"oot-{args.oot_version}.map"
+
+    mapPath = BUILTMAP
+    if args.use_expected:
+        mapPath = "expected" / BUILTMAP
+
+    mapfile_parser.frontends.sym_info.doSymInfo(mapPath, args.symname)
+
+if __name__ == "__main__":
+    symInfoMain()
